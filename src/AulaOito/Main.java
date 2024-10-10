5e3f39347ed667d6502c40e58462ebd8dbcbd7d0
fernandes-ez <enzo.dantas@hotmail.com>
2024-10-10 10:50:38 -0300

    Correção Aula 8

--- src/AulaOito/Main.java
+++ src/AulaOito/Main.java
@@ -4,13 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Fila fila = new Fila();
         System.out.printf(fila.estaVazia() ? "Vazia": "Não vazia");
-        System.out.println("\nInício "+fila.inicio());
-
         fila.enfileirar(1);
         fila.enfileirar(2);
         fila.enfileirar(3);
         System.out.printf(fila.estaVazia() ? "Vazia": "Não vazia");
-        Object info;
+        System.out.println("\nInício "+fila.inicio());
+
+
+        System.out.printf(fila.estaVazia() ? "Vazia": "Não vazia");
+        Object info = fila.desenfileirar();
+        while (info != null){
+            System.out.println(info);
+            info = fila.desenfileirar();
+        }
+        System.out.printf(fila.estaVazia() ? "Vazia": "Não vazia");
+        System.out.println("\nInício "+fila.inicio());
+
+        fila.enfileirar(4);
+        fila.enfileirar(5);
+        fila.enfileirar(6);
+        System.out.printf(fila.estaVazia() ? "Vazia": "Não vazia");
+        System.out.println("\nInício "+fila.inicio());
+
         info = fila.desenfileirar();
         while (info != null){
             System.out.println(info);
@@ -18,6 +33,8 @@ public class Main {
         }
         System.out.printf(fila.estaVazia() ? "Vazia": "Não vazia");
         System.out.println("\nInício "+fila.inicio());
+
+
 
 
     }
